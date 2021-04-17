92fbeb77971c97ff4a9727ca93150237a11df3d1
leovcunha <lvcunha@gmail.com>
2021-04-17 17:19:47 -0300

    Update io.py

--- io.py
+++ io.py
@@ -3,6 +3,8 @@
 # OUT = output directory
 
 def savepic(image_id, IN, OUT):
+    if not os.path.isdir(OUT):
+        os.mkdir(OUT)
     for _id in image_id:
         img = cv2.imread(os.path.join(IN, _id))        
-        cv2.imwrite("{}/{}".format(myoutput, _id), img)
+        cv2.imwrite("{}/{}".format(OUT, _id), img)
